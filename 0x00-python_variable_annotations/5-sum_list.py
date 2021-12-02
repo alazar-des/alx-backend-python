d3bbd5fcd796277781b2c2b9a46232a615b92a10
alazar-des <alazar.des@gmail.com>
2021-12-02 13:14:56 +0300

    type annonated sum of list function

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" type annonated sum of float of lists module. """
+from typing import List
+
+
+def sum_list(floats: List[float]) -> float:
+    """ sum of lists. """
+    return sum(floats)
