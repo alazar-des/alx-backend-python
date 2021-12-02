d793b47b3cbf4d566d293ba025fa33aff9e81efc
alazar-des <alazar.des@gmail.com>
2021-12-02 13:55:38 +0300

    return callable

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" a module that return annonated function. """
+from typing import Callable
+
+
+def func(mul: float) -> float:
+    """ return square of mul. """
+    return mul * mul
+
+
+def make_multiplier(mul: float) -> Callable[[float], float]:
+    """ return a callable. """
+    return func
