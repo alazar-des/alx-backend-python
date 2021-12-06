038f0f0f55830f3016c21adf19c31a6458a5da7a
alazar-des <alazar.des@gmail.com>
2021-12-06 15:10:41 +0300

    basic async function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" async function. """
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ async funcion that waits asyncly betweet 0 and max_delay."""
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
